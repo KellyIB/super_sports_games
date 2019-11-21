df6ccdd3337e3602f973d87041ccd03567f3a1d1
Kelly Bard <kellyibard@gmail.com>
2019-11-21 15:09:22 -0700

    Modified File

--- lib/event.rb
+++ lib/event.rb
@@ -7,12 +7,23 @@ class Event
     @ages = []
   end
 
-  def max_age(ages)
+  def max_age
     p ages.max_age
   end
 
-  def min_age(ages)
+  def min_age
     p ages.min_age
   end
-  
+
+  def average_age
+    p ages.average_age.round(2)
+  end
+
+  def standard_deviation_age
+    p standard_deviation_age.round(2)
+  end
+
+
+
+
 end
