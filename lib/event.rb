e83c10ffeb0464241ecab47c93655e6fcce19a87
Kelly Bard <kellyibard@gmail.com>
2019-11-21 15:40:07 -0700

    Modified file

--- lib/event.rb
+++ lib/event.rb
@@ -2,24 +2,38 @@ class Event
 
   attr_reader(:name, :ages)
 
-  def initialize(name, ages)
+  def initialize(name, [])
     @name = name
     @ages = []
   end
 
   def max_age
+    max_age = ages.max
     p ages.max_age
   end
 
   def min_age
+    min_age = ages.min
     p ages.min_age
   end
 
   def average_age
-    p ages.average_age.round(2)
+    x = ages.count
+    sum_ages = 0
+
+    ages.each do |age|
+      sum_ages += age
+    end
+
+    average_age = sum_ages/x.to_f.round(2)
+  end
+
+  p ages.average_age
+
   end
 
   def standard_deviation_age
+    
     p standard_deviation_age.round(2)
   end
 
