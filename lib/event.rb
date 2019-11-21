eca53a152ab5162963abf9e448278d8fe9e5da5f
Kelly Bard <kellyibard@gmail.com>
2019-11-21 15:56:57 -0700

    Added changes

--- lib/event.rb
+++ lib/event.rb
@@ -34,10 +34,30 @@ class Event
 
   def standard_deviation_age
 
-    p standard_deviation_age.round(2)
+    sum_ages = 0
+
+    sum_temp2 = 0
+
+    ages.each do |age|
+      sum_ages += age
+    end
+
+    total_ages = ages.count
+
+    mean = sum_ages/total_ages
+
+    ages.each do |age|
+      temp = (age - mean)
+      temp2 = temp * temp
+
+      sum_temp2 += temp2
+    end
+
+    temp3 = sum_temp2/total_ages
+
+    st_dev = Math.sqrt(temp3)
+
+    p st_dev.round(2)
   end
-
-
-
 
 end
