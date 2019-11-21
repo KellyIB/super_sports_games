b7109df939e4738a2b4502d7b77907be1da8a411
Kelly Bard <kellyibard@gmail.com>
2019-11-21 13:52:54 -0700

    wrote stdev file started event

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,5 +1,51 @@
 ages = [24, 30, 18, 20, 41]
 
+sum_ages = 0
+
+sum_temp2 = 0
+
+ages.each do |age|
+  sum_ages += age
+end
+
+total_ages = ages.count
+
+mean = sum_ages/total_ages
+
+ages.each do |age|
+  temp = (age - mean)
+  temp2 = temp * temp
+
+  sum_temp2 += temp2
+end
+
+temp3 = sum_temp2/total_ages
+
+st_dev = Math.sqrt(temp3)
+
+p st_dev
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Your code here for calculating the standard deviation
 
 # When you find the standard deviation, print it out
